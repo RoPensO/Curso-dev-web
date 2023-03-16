ce6431669c4167798e7c21c13c84b01216924e9f
RoPensO <ropenso@gmail.com>
2023-03-16 17:57:07 -0300

    React-JS

---
+++ reactJS/primeira-pagina/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <h1>Hello world</h1>
+    <p>Com <span className="react">React JS</span></p>
+    <p className="autor">Criado por RoPensO</p>
+   </div>
+  );
+}
+ 
+export default App;
