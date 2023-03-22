c0efa7a7fe6c4649ac31c296c0afb0642656d45a
RoPensO <ropenso@gmail.com>
2023-03-22 14:32:14 -0300

    ReactJs

---
+++ reactJS/exeplos práticos/exemplo-02/src/componentes/HomePage/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Card from '../Card/';
+
+import './style.css'
+
+export default function HomePage() {
+  return (
+    <div className='HomePage'>
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
+}
