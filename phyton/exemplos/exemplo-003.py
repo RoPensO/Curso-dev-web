f2e1dd72211dea9b1c4d1cd56dd1f6b9f6414c31
RoPensO <ropenso@gmail.com>
2023-03-29 16:51:47 -0300

    exercicios python

---
+++ phyton/exemplos/exemplo-003.py
@@ -0,0 +1,5 @@
+from math import floor
+
+num = float(input('Digite um número inteiro qualquer usando vírgula:'))
+
+print('o número escolhido é {} e sua versão arendondada pra baixo é {}'.format(num, floor(num)))
---
+++ phyton/exercicios/ex016.py
@@ -0,0 +1,12 @@
+# com importação de um módulo math
+from math import trunc
+num = float(input('Digite um número real qualquer: '))
+
+print(trunc(num))
+
+
+
+# versão nativa sem importação de nada
+
+# num = float(input('Digite um número real qualquer: '))
+# print(int(num))
