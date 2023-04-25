bce412cb5605a5d47f2a5f502cf182bf06c5e1d4
RoPensO <ropenso@gmail.com>
2023-04-25 14:43:48 -0300

    atualização de arquivos

--- reactJS/exeplos práticos/exemplo-03/src/App.js
+++ reactJS/exeplos práticos/exemplo-03/src/App.js
@@ -1,12 +1,10 @@
-import React from 'react';
-import Rotas from './componente/Routes';
-
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Rotas />
+     
     </div>
   );
 }
