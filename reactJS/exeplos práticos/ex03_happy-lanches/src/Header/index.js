fd626b93915d841d4a81ee6fbfdbd171aabf71e3
RoPensO <ropenso@gmail.com>
2023-05-05 12:11:58 -0300

    happylanches react

---
+++ reactJS/exeplos práticos/ex03_happy-lanches/src/Header/index.js
@@ -0,0 +1,11 @@
+import './header.css';
+ 
+function Header() {
+    return (
+        <div className='topo'>
+            <div class="logo">Happy <span>Lanches</span></div>
+        </div>
+    );
+}
+
+export default Header;
